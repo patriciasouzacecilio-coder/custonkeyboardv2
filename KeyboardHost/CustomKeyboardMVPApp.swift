2629916146a713d65e83b5942e303f02f8e47b0a
patriciasouzacecilio-coder <patriciasouzacecilio@gmail.com>
2026-09-05 10:21:43 -0300

    Create CustomKeyboardMVPApp.swift

---
+++ KeyboardHost/CustomKeyboardMVPApp.swift
@@ -0,0 +1,11 @@
+import SwiftUI
+
+@main
+struct CustomKeyboardMVPApp: App {
+    var body: some Scene {
+        WindowGroup {
+            Text("Custom Keyboard")
+                .padding()
+        }
+    }
+}
